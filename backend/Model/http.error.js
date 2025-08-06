460e8f631cea10a4361953fc07ac20aad1eafab2
Equis Dev <dvaghasiya766@rku.ac.in>
2025-08-06 14:08:28 +0530

    HTTP Error Class

--- backend/Model/http.error.js
+++ backend/Model/http.error.js
@@ -1,8 +1,10 @@
-class httpError extends Error {
-  constructor(message, errCode) {
-    super(message); //Add a 'message' property
-    this.code = errCode;
+// Custom HTTP Error class that extends the built-in Error class
+class HttpError extends Error {
+  constructor(message, errorCode) {
+    super(message); // Call the parent constructor with the message
+    this.code = errorCode; // Add a custom 'code' property to store HTTP status code
   }
 }
 
-module.exports = httpError;
+// Export the class so it can be used in other files
+module.exports = HttpError;
